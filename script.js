36ab116ea81743481526c272e0a85ec282c6e395
mguser4564 <mmtravel021@gmail.com>
2023-02-15 16:43:27 +0300

    set up css layout

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ const gridContainer = document.getElementById('gridContainer');
 const toggleButton = document.getElementById('toggle-size');
 
 let gridSize = 16;
+let gridItems = [];
 
 function createGrid() {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
         const div = document.createElement('div');
-        div.classList.add('gridItem');
+        div.classList.add('gridItems');
         gridContainer.appendChild(div);
+        gridItems.push(div);
         }
     }
   }
 
   createGrid();
 
+  function toggleGrid(){
   toggleButton.addEventListener('click', () => {
     gridContainer.innerHTML = '';
+    gridItems = [];
     if (gridSize === 16) {
         gridContainer.style.gridTemplateRows = 'repeat(32, 1fr)'
         gridContainer.style.gridTemplateColumns = 'repeat(32, 1fr)'
@@ -32,4 +36,12 @@ function createGrid() {
       gridSize = 16;
     }
     createGrid();
-  });
+  });};
+  
+  toggleGrid()
+ 
+    gridItems.forEach(element => {
+        element.addEventListener('click', () => {
+            element.style.backgroundColor = 'red';  
+        });
+    });
