b5239409f59bf22237b1195bfcfc540fcba74508
mguser4564 <mmtravel021@gmail.com>
2023-02-16 02:20:32 +0300

    add a clear button

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
 
 const gridContainer = document.getElementById('gridContainer');
 const slider = document.getElementById('mySlider');
-
+const clear = document.getElementById('clearGrid')
 const output = document.getElementById("sliderValue");
-output.innerHTML = `${slider.value} x ${slider.value}`;
-slider.oninput = function() {
-  output.innerHTML = `${this.value} x ${this.value}` 
-}
 
 let gridSize = slider.value;
 let gridItems = [];
 
 function createGrid() {
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-        const div = document.createElement('div');
-        div.classList.add('gridItems');
-        gridContainer.appendChild(div);
-        gridItems.push(div);
-        }
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const div = document.createElement('div');
+      div.classList.add('gridItems');
+      gridContainer.appendChild(div);
+      gridItems.push(div);
     }
   }
+}
 
-  function slideGrid(){
-    slider.addEventListener('input', () => {
+function slideGrid() {
+  slider.addEventListener('input', () => {
     gridContainer.innerHTML = '';
     gridSize = slider.value;
     gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
     createGrid();
     changeColor();
-  });};
- 
-  function changeColor (){
-    gridItems.forEach(element => {
-        element.addEventListener('mousemove', () => {
-            element.style.backgroundColor = 'red';  
-        });
-    });};
+  });
+};
 
+function sliderValueDisplay (){
+  output.innerHTML = `${slider.value} x ${slider.value}`;
+  slider.oninput = function () {
+    output.innerHTML = `${this.value} x ${this.value}`
+  }};
+
+function changeColor() {
+  gridItems.forEach(element => {
+    element.addEventListener('mousemove', () => {
+      element.style.backgroundColor = 'black';
+    });
+  });
+};
+
+function clearGrid() {
+  clear.addEventListener('click', () => {
+    gridContainer.innerHTML = '';
+    gridSize = slider.value;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
     createGrid();
-    slideGrid();
     changeColor();
+  })
+};
+
+createGrid();
+slideGrid();
+sliderValueDisplay ();
+changeColor();
+clearGrid();
